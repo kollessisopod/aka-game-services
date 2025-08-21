79b3bcaa9cf42b18c23a53954cb2640be9fa0439
K. Ata Turkoglu <turkoglu.kata@gmail.com>
2025-08-21 20:51:28 +0300

    Auto-generated commit

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    baseUrlStorage: "https://localhost:7221",
-    baseUrlMaster: 'https://aka-game-services.kolless.xyz'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    baseUrlStorage: "none",
-    baseUrlMaster: 'https://aka-game-services.kolless.xyz'
-};
